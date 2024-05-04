a0ebd754eb362c0e8702f6677532027ce40adb71
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-05-04 11:59:10 -0500

    cambios

--- src/main/java/com/centroinformacion/util/AppSettings.java
+++ src/main/java/com/centroinformacion/util/AppSettings.java
@@ -12,7 +12,26 @@ public class AppSettings {
 	public static final int CATALOGO_06_TIPO_DE_SALA = 6;
 	public static final int CATALOGO_07_SEDE = 7;
 	public static final int CATALOGO_08_ESTADO_DE_LIBRO = 8;
+	public static final int CATALOGO_09_ESTADO_SALA= 9;
+	public static final int CATALOGO_10_CENTRO_DE_ESTUDIOS = 10;
+	public static final int CATALOGO_11_IDIOMA = 11;
+	public static final int CATALOGO_12_TEMA_DE_TESIS = 12;
 	
 	public static final int ACTIVO = 1;
 	public static final int INACTIVO = 0;
+	
+	public static final String MENSAJE_REG_YA_EXISTE = "Ya existe dicho registro.";
+	public static final String MENSAJE_REG_ERROR = "No se registró, consulte con el administrador.";
+	public static final String MENSAJE_REG_EXITOSO = "Se registró correctamente.";
+		
+	public static final String MENSAJE_ACT_YA_EXISTE = "Ya existe dicho registro.";
+	public static final String MENSAJE_ACT_ERROR = "No se actualizó, consulte con el administrador.";
+	public static final String MENSAJE_ACT_EXITOSO = "Se actualizó correctamente.";
+	public static final String MENSAJE_ACT_NO_EXISTE_ID = "No existe el ID que se desea actualizar.";
+	
+	public static final String MENSAJE_ELI_ERROR = "No se eliminó, ya que el registro esta relacionado.";
+	public static final String MENSAJE_ELI_EXITOSO = "Se eliminó correctamente.";
+	public static final String MENSAJE_ELI_NO_EXISTE_ID = "No existe el ID que se desea eliminar.";
+	
+	public static final String MENSAJE_DNI_YA_EXISTE = "Ya existe dicho el DNI : ";
 }
